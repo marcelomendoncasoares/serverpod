0d43ea6ccf8e29120078566a5eaa0ee6f0a4413a
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-05-04 22:51:06 -0300

    test: Add new models and tests to `serverpod_test_server`

---
+++ tests/serverpod_test_server/migrations/20250502024421330/migration.sql
@@ -0,0 +1,79 @@
+BEGIN;
+
+--
+-- Function: gen_random_uuid_v7()
+-- Source: https://gist.github.com/kjmph/5bd772b2c2df145aa645b837da7eca74
+-- License: MIT (copyright notice included on the generator source code).
+--
+create or replace function gen_random_uuid_v7()
+returns uuid
+as $$
+begin
+  -- use random v4 uuid as starting point (which has the same variant we need)
+  -- then overlay timestamp
+  -- then set version 7 by flipping the 2 and 1 bit in the version 4 string
+  return encode(
+    set_bit(
+      set_bit(
+        overlay(uuid_send(gen_random_uuid())
+                placing substring(int8send(floor(extract(epoch from clock_timestamp()) * 1000)::bigint) from 3)
+                from 1 for 6
+        ),
+        52, 1
+      ),
+      53, 1
+    ),
+    'hex')::uuid;
+end
+$$
+language plpgsql
+volatile;
+
+--
+-- ACTION CREATE TABLE
+--
+CREATE TABLE "object_with_vector" (
+    "id" bigserial PRIMARY KEY,
+    "vector" vector(512) NOT NULL,
+    "vectorNullable" vector(512)
+);
+
+--
+-- ACTION ALTER TABLE
+--
+ALTER TABLE "types" ADD COLUMN "aVector" vector(3);
+
+--
+-- MIGRATION VERSION FOR serverpod_test
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_test', '20250502024421330', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20250502024421330', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20240516151843329', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth', '20240520102713718', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240520102713718', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_test_module
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_test_module', '20241219152628926', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20241219152628926', "timestamp" = now();
+
+
+COMMIT;
