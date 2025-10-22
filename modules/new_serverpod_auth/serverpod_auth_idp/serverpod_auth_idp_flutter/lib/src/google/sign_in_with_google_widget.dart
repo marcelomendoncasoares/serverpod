2e054602f609bc89fb244fb5821d3f3ca43566e8
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-10-21 22:44:14 -0300

    feat: Complete Google implementation

--- modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/sign_in_with_google_widget.dart
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/sign_in_with_google_widget.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:serverpod_auth_core_flutter/serverpod_auth_core_flutter.dart';
 import 'package:serverpod_auth_idp_client/serverpod_auth_idp_client.dart';
+
+import 'sign_in_service.dart';
+import 'web/button.dart';
 
 /// A widget that provides Google Sign-In functionality for all platforms.
 ///
@@ -35,16 +40,27 @@ class SignInWithGoogleWidget extends StatefulWidget {
   /// Callback when an error occurs during authentication.
   final Function(Object error)? onError;
 
-  /// Optional Google Sign-In configuration.
-  /// If not provided, uses default configuration.
-  final GoogleSignIn? googleSignIn;
+  /// A styled button to use for the web platform.
+  final GoogleSignInWebButton? webButton;
+
+  /// Whether to attempt to authenticate the user automatically using the
+  /// `attemptLightweightAuthentication` method after the widget is initialized.
+  ///
+  /// The amount of allowable UI is up to the platform to determine, but it
+  /// should be minimal. Possible examples include FedCM on the web, and One Tap
+  /// on Android. Platforms may even show no UI, and only sign in if a previous
+  /// sign-in is being restored. This method is intended to be called as soon
+  /// as the application needs to know if the user is signed in, often at
+  /// initial launch.
+  final bool attemptLightweightSignIn;
 
   /// Creates a Google Sign-In widget.
   const SignInWithGoogleWidget({
     required this.client,
     this.onAuthenticated,
     this.onError,
-    this.googleSignIn,
+    this.webButton,
+    this.attemptLightweightSignIn = true,
     super.key,
   });
 
@@ -53,30 +69,86 @@ class SignInWithGoogleWidget extends StatefulWidget {
 }
 
 class _SignInWithGoogleWidgetState extends State<SignInWithGoogleWidget> {
-  late final GoogleSignIn _googleSignIn;
   bool _isLoading = false;
-  String? _errorMessage;
+  bool _isInitialized = false;
+  StreamSubscription<GoogleSignInAuthenticationEvent?>? _authSubscription;
 
   @override
   void initState() {
     super.initState();
-    _googleSignIn = widget.googleSignIn ?? GoogleSignIn.instance;
+
+    unawaited(
+      GoogleSignInService.instance
+          .ensureInitialized(auth: widget.client.auth)
+          .then((signIn) {
+        _authSubscription = signIn.authenticationEvents.listen(
+          _handleAuthenticationEvent,
+          onError: _handleAuthenticationError,
+        );
+
+        if (widget.attemptLightweightSignIn) {
+          unawaited(signIn.attemptLightweightAuthentication());
+        }
+
+        if (mounted) {
+          setState(() {
+            _isInitialized = true;
+          });
+        }
+      }),
+    );
   }
 
-  Future<void> _handleSignIn() async {
+  @override
+  void dispose() {
+    unawaited(_authSubscription?.cancel());
+    super.dispose();
+  }
+
+  void _setLoading(bool value) {
     setState(() {
-      _isLoading = true;
-      _errorMessage = null;
+      _isLoading = value;
     });
+  }
+
+  Future<void> _handleAuthenticationEvent(
+    GoogleSignInAuthenticationEvent googleAuthEvent,
+  ) async {
+    switch (googleAuthEvent) {
+      case GoogleSignInAuthenticationEventSignIn(user: final user):
+        await _handleServerSideSignIn(idToken: user.authentication.idToken);
+      case GoogleSignInAuthenticationEventSignOut():
+        await widget.client.auth.signOutDevice();
+    }
+  }
+
+  Future<void> _handleAuthenticationError(Object error) async {
+    widget.onError?.call(error);
+  }
+
+  Future<void> _initiateSignIn() async {
+    _setLoading(true);
+
+    if (!GoogleSignIn.instance.supportsAuthenticate()) {
+      throw StateError('This sign-in method is not supported on this platform');
+    }
 
     try {
-      final GoogleSignInAccount account = await _googleSignIn.authenticate();
+      final account = await GoogleSignIn.instance.authenticate();
+      await _handleServerSideSignIn(idToken: account.authentication.idToken);
+    } catch (e) {
+      _setLoading(false);
+      widget.onError?.call(e);
+    }
+  }
 
-      final GoogleSignInAuthentication auth = account.authentication;
-      final String? idToken = auth.idToken;
-
+  Future<void> _handleServerSideSignIn({required String? idToken}) async {
+    try {
       if (idToken == null) {
-        throw Exception('Failed to obtain ID token from Google');
+        throw GoogleSignInException(
+          code: GoogleSignInExceptionCode.unknownError,
+          description: 'Failed to obtain ID token from Google',
+        );
       }
 
       final endpoint = widget.client.getEndpointOfType<EndpointGoogleIDPBase>();
@@ -84,17 +156,10 @@ class _SignInWithGoogleWidgetState extends State<SignInWithGoogleWidget> {
 
       await widget.client.auth.updateSignedInUser(authSuccess);
 
-      setState(() {
-        _isLoading = false;
-      });
-
+      _setLoading(false);
       widget.onAuthenticated?.call();
     } catch (error) {
-      setState(() {
-        _isLoading = false;
-        _errorMessage = error.toString();
-      });
-
+      _setLoading(false);
       widget.onError?.call(error);
     }
   }
@@ -105,38 +170,34 @@ class _SignInWithGoogleWidgetState extends State<SignInWithGoogleWidget> {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        ElevatedButton.icon(
-          onPressed: _isLoading ? null : _handleSignIn,
-          icon: _isLoading
-              ? const SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(strokeWidth: 2),
-                )
-              : Image.asset(
-                  'assets/images/google.png',
-                  package: 'serverpod_auth_idp_flutter',
-                  height: 24,
-                  width: 24,
-                ),
-          label: Text(_isLoading ? 'Signing in...' : 'Sign in with Google'),
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black87,
-          ),
-        ),
-        if (_errorMessage != null)
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: Text(
-              _errorMessage!,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.error,
-                fontSize: 12,
-              ),
+        if (GoogleSignIn.instance.supportsAuthenticate())
+          ElevatedButton.icon(
+            onPressed: _isLoading ? null : _initiateSignIn,
+            icon: _isLoading
+                ? const SizedBox(
+                    width: 16,
+                    height: 16,
+                    child: CircularProgressIndicator(strokeWidth: 2),
+                  )
+                : Image.asset(
+                    'assets/images/google.png',
+                    package: 'serverpod_auth_idp_flutter',
+                    height: 24,
+                    width: 24,
+                  ),
+            iconAlignment: IconAlignment.start,
+            label: Text(_isLoading ? 'Signing in...' : 'Continue with Google'),
+            // NOTE: Styled according to official guidelines.
+            // https://developers.google.com/identity/branding-guidelines#padding
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black87,
+              shape: StadiumBorder(),
             ),
-          ),
+          )
+        else if (_isInitialized)
+          widget.webButton ?? GoogleSignInWebButton()
       ],
     );
   }
---
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/web/button.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+
+import 'wrapper.dart';
+
+// Convenience export of the button configuration enums.
+export 'wrapper.dart'
+    show
+        GSIButtonType,
+        GSIButtonTheme,
+        GSIButtonSize,
+        GSIButtonText,
+        GSIButtonShape,
+        GSIButtonLogoAlignment;
+
+/// A widget that renders the Google Sign-In button for web.
+class GoogleSignInWebButton extends StatelessWidget {
+  /// The button type: icon, or standard button.
+  final GSIButtonType type;
+
+  /// The button theme.
+  ///
+  /// For example, filledBlue or filledBlack.
+  final GSIButtonTheme theme;
+
+  /// The button size.
+  ///
+  /// For example, small or large.
+  final GSIButtonSize size;
+
+  /// The button text.
+  ///
+  /// For example "Sign in with Google" or "Sign up with Google".
+  final GSIButtonText text;
+
+  /// The button shape.
+  ///
+  /// For example, rectangular or circular.
+  final GSIButtonShape shape;
+
+  /// The Google logo alignment: left or center.
+  final GSIButtonLogoAlignment logoAlignment;
+
+  /// The minimum button width, in pixels.
+  ///
+  /// The maximum width is 400 pixels.
+  final double minimumWidth;
+
+  /// The pre-set locale of the button text.
+  ///
+  /// If not set, the browser's default locale or the Google session user's
+  /// preference is used.
+  ///
+  /// Different users might see different versions of localized buttons, possibly
+  /// with different sizes.
+  final String? locale;
+
+  /// A wrapper function to the rendered button to ensure style consistency.
+  ///
+  /// This wrapper ensures the consistency of the rendered button with the rest
+  /// of the application. Since the render configuration is done through enum
+  /// values, the wrapper will be called with a [GoogleSignInStyle] object that
+  /// translates the enum values to actual style properties. The [Widget] is the
+  /// rendered Google button that should be wrapped.
+  ///
+  /// Be mindful that creating the button with no wrapper will also result in a
+  /// dangling "Getting ready..." text that is returned while the iFrame is
+  /// being built.
+  final Widget Function(GoogleSignInStyle style, Widget child)? buttonWrapper;
+
+  /// Creates a Google Sign-In button for web.
+  const GoogleSignInWebButton({
+    this.type = GSIButtonType.standard,
+    this.theme = GSIButtonTheme.outline,
+    this.size = GSIButtonSize.large,
+    this.text = GSIButtonText.continueWith,
+    this.shape = GSIButtonShape.pill,
+    this.logoAlignment = GSIButtonLogoAlignment.left,
+    this.minimumWidth = 240,
+    this.locale,
+    this.buttonWrapper = wrapAsMaterial,
+    super.key,
+  }) : assert(
+          minimumWidth > 0 && minimumWidth <= 400,
+          'Invalid minimumWidth. Must be between 0 and 400.',
+        );
+
+  /// Builds Google Sign-In button with the icon type.
+  factory GoogleSignInWebButton.icon() =>
+      GoogleSignInWebButton(type: GSIButtonType.icon);
+
+  /// Builds Google Sign-In button compatible with Material's filled button.
+  factory GoogleSignInWebButton.filled({
+    GSIButtonTheme theme = GSIButtonTheme.outline,
+    GSIButtonSize size = GSIButtonSize.large,
+    GSIButtonText text = GSIButtonText.continueWith,
+    GSIButtonShape shape = GSIButtonShape.pill,
+    GSIButtonLogoAlignment logoAlignment = GSIButtonLogoAlignment.left,
+    double minimumWidth = 240,
+  }) =>
+      GoogleSignInWebButton(
+        theme: theme,
+        size: size,
+        text: text,
+        shape: shape,
+        logoAlignment: logoAlignment,
+        minimumWidth: minimumWidth,
+        buttonWrapper: wrapAsFilledButton,
+      );
+
+  /// Builds Google Sign-In button compatible with Material's outline button.
+  factory GoogleSignInWebButton.outlined({
+    GSIButtonSize size = GSIButtonSize.large,
+    GSIButtonText text = GSIButtonText.continueWith,
+    GSIButtonShape shape = GSIButtonShape.pill,
+    GSIButtonLogoAlignment logoAlignment = GSIButtonLogoAlignment.left,
+    double minimumWidth = 240,
+  }) =>
+      GoogleSignInWebButton(
+        theme: GSIButtonTheme.outline,
+        size: size,
+        text: text,
+        shape: shape,
+        logoAlignment: logoAlignment,
+        minimumWidth: minimumWidth,
+        buttonWrapper: wrapAsOutline,
+      );
+
+  /// Builds Google Sign-In button compatible with Material's elevated button.
+  factory GoogleSignInWebButton.elevated({
+    GSIButtonTheme theme = GSIButtonTheme.outline,
+    GSIButtonSize size = GSIButtonSize.large,
+    GSIButtonText text = GSIButtonText.continueWith,
+    GSIButtonShape shape = GSIButtonShape.pill,
+    GSIButtonLogoAlignment logoAlignment = GSIButtonLogoAlignment.left,
+    double minimumWidth = 240,
+  }) =>
+      GoogleSignInWebButton(
+        theme: theme,
+        size: size,
+        text: text,
+        shape: shape,
+        logoAlignment: logoAlignment,
+        minimumWidth: minimumWidth,
+        buttonWrapper: wrapAsElevatedButton,
+      );
+
+  /// Wraps the button to match Material's default button style.
+  static Widget wrapAsMaterial(GoogleSignInStyle style, Widget child) {
+    return Material(
+      borderRadius: style.borderRadius,
+      clipBehavior: Clip.antiAlias,
+      child: Container(
+        alignment: Alignment.center,
+        color: style.backgroundColor,
+        width: style.size.width,
+        height: style.size.height,
+        child: child,
+      ),
+    );
+  }
+
+  /// Wraps the button to match Material's filled button style.
+  static Widget wrapAsFilledButton(GoogleSignInStyle style, Widget child) {
+    return FilledButton(
+      onPressed: () {},
+      style: FilledButton.styleFrom(
+        padding: EdgeInsets.zero,
+        fixedSize: style.size,
+        backgroundColor: style.backgroundColor,
+        shape: RoundedRectangleBorder(borderRadius: style.borderRadius),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: child,
+    );
+  }
+
+  /// Wraps the button to match Material's outline button style.
+  static Widget wrapAsOutline(GoogleSignInStyle style, Widget child) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        padding: EdgeInsets.zero,
+        fixedSize: style.size,
+        backgroundColor: style.backgroundColor,
+        shape: RoundedRectangleBorder(borderRadius: style.borderRadius),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: child,
+    );
+  }
+
+  /// Wraps the button to match Material's elevated button style.
+  static Widget wrapAsElevatedButton(GoogleSignInStyle style, Widget child) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.zero,
+        fixedSize: style.size,
+        backgroundColor: style.backgroundColor,
+        shape: RoundedRectangleBorder(borderRadius: style.borderRadius),
+      ),
+      clipBehavior: Clip.antiAlias,
+      child: child,
+    );
+  }
+
+  /// Render the button with the actual width.
+  Widget _renderButton({double? width}) => renderButton(
+        configuration: GSIButtonConfiguration(
+          type: type,
+          theme: theme,
+          size: size,
+          text: text,
+          shape: shape,
+          logoAlignment: logoAlignment,
+          minimumWidth: width ?? minimumWidth,
+          locale: locale,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    if (type == GSIButtonType.icon) {
+      return _renderButton();
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      final width = constraints.maxWidth.clamp(minimumWidth, 400).toDouble();
+
+      final buttonStyle = GoogleSignInStyle.fromConfiguration(
+        theme: theme,
+        shape: shape,
+        size: size,
+        width: width,
+      );
+
+      final button = _renderButton(width: width);
+      return buttonWrapper?.call(buttonStyle, button) ?? button;
+    });
+  }
+}
+
+/// The style of the rendered Google button.
+class GoogleSignInStyle {
+  final Size size;
+  final Color backgroundColor;
+  final BorderRadius borderRadius;
+
+  /// Creates a [GoogleSignInStyle] with the given properties.
+  const GoogleSignInStyle({
+    required this.size,
+    required this.backgroundColor,
+    required this.borderRadius,
+  });
+
+  /// Creates a [GoogleSignInStyle] from the button configuration.
+  ///
+  /// Values are translated from the enum values to actual style properties
+  /// using the Google Sign-In documentation as reference.
+  factory GoogleSignInStyle.fromConfiguration({
+    required GSIButtonTheme theme,
+    required GSIButtonShape shape,
+    required GSIButtonSize size,
+    required double width,
+  }) {
+    final height = switch (size) {
+      GSIButtonSize.large => 40.0,
+      GSIButtonSize.medium => 32.0,
+      GSIButtonSize.small => 20.0,
+    };
+
+    return GoogleSignInStyle(
+      size: Size(width, height),
+      backgroundColor: switch (theme) {
+        GSIButtonTheme.outline => Colors.white,
+        GSIButtonTheme.filledBlue => Colors.blue,
+        GSIButtonTheme.filledBlack => Colors.black,
+      },
+      borderRadius: switch (shape) {
+        GSIButtonShape.rectangular => BorderRadius.circular(4),
+        GSIButtonShape.pill => BorderRadius.circular(height / 2),
+      },
+    );
+  }
+}
