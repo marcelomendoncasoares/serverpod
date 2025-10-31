d536c800006a95788af933316ada92e9fa5770d6
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-10-31 00:23:25 -0300

    feat: Add a storybook to assert correct implementation of auth IDP UI components

---
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/storybook/lib/stories/google.dart
@@ -0,0 +1,139 @@
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'package:storybook_toolkit/storybook_toolkit.dart';
+
+import '../utils/story.dart';
+
+final googleStories = [
+  Story(
+    name: 'Google/Button Styles',
+    description:
+        'Google Sign-In native and web button styles side-by-side. The first '
+        'row is the native button, the second row is the web button.',
+    builder: (context) {
+      final fillStyle = context.knobs.options<GSIButtonTheme>(
+        label: 'Fill style',
+        options: GSIButtonTheme.values.asOptions(),
+        initial: GSIButtonTheme.outline,
+      );
+
+      final size = context.knobs.options<GSIButtonSize>(
+        label: 'Size',
+        options: GSIButtonSize.values.asOptions(),
+        initial: GSIButtonSize.large,
+      );
+
+      final shape = context.knobs.options<GSIButtonShape>(
+        label: 'Shape',
+        options: GSIButtonShape.values.asOptions(),
+        initial: GSIButtonShape.pill,
+      );
+
+      final text = context.knobs.options<GSIButtonText>(
+        label: 'Text',
+        options: GSIButtonText.values.asOptions(),
+        initial: GSIButtonText.continueWith,
+      );
+
+      final logoAlignment = context.knobs.options<GSIButtonLogoAlignment>(
+        label: 'Logo alignment',
+        options: GSIButtonLogoAlignment.values.asOptions(),
+        initial: GSIButtonLogoAlignment.left,
+      );
+
+      return buildIsolatedElementsForStory(context, {
+        'Material': [
+          const GoogleSignInNativeButton(
+            onPressed: _nullCallback,
+            isLoading: false,
+            isDisabled: false,
+          ),
+          GoogleSignInWebButton(
+            theme: fillStyle,
+            size: size,
+            text: text,
+            shape: shape,
+            logoAlignment: logoAlignment,
+          ),
+        ],
+        'Elevated': [
+          const GoogleSignInNativeButton(
+            onPressed: _nullCallback,
+            isLoading: false,
+            isDisabled: false,
+          ),
+          GoogleSignInWebButton.elevated(
+            theme: fillStyle,
+            size: size,
+            text: text,
+            shape: shape,
+            logoAlignment: logoAlignment,
+          ),
+        ],
+        'Filled': [
+          const GoogleSignInNativeButton(
+            onPressed: _nullCallback,
+            isLoading: false,
+            isDisabled: false,
+          ),
+          GoogleSignInWebButton.filled(
+            theme: fillStyle,
+            size: size,
+            text: text,
+            shape: shape,
+            logoAlignment: logoAlignment,
+          ),
+        ],
+        'Outlined': [
+          const GoogleSignInNativeButton(
+            onPressed: _nullCallback,
+            isLoading: false,
+            isDisabled: false,
+          ),
+          GoogleSignInWebButton.outlined(
+            size: size,
+            text: text,
+            shape: shape,
+            logoAlignment: logoAlignment,
+          ),
+        ],
+        'Icon': [
+          const GoogleSignInNativeButton(
+            onPressed: _nullCallback,
+            isLoading: false,
+            isDisabled: false,
+          ),
+          GoogleSignInWebButton.icon(),
+        ],
+      });
+    },
+  ),
+  Story(
+    name: 'Google/Native Button States',
+    description: 'Google Sign-In native button states side-by-side.',
+    builder: (context) => buildIsolatedElementsForStory(context, {
+      'Idle': [
+        const GoogleSignInNativeButton(
+          onPressed: _nullCallback,
+          isLoading: false,
+          isDisabled: false,
+        ),
+      ],
+      'Loading': [
+        const GoogleSignInNativeButton(
+          onPressed: _nullCallback,
+          isLoading: true,
+          isDisabled: false,
+        ),
+      ],
+      'Disabled': [
+        const GoogleSignInNativeButton(
+          onPressed: _nullCallback,
+          isLoading: false,
+          isDisabled: true,
+        ),
+      ],
+    }),
+  ),
+];
+
+void _nullCallback() {}
---
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/storybook/lib/utils/story.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:storybook_toolkit/storybook_toolkit.dart';
+import 'package:serverpod_auth_idp_flutter/widgets.dart';
+
+Widget buildIsolatedElementsForStory(
+  BuildContext context,
+  Map<String, List<Widget>> elements,
+) {
+  final showTitles = context.knobs.boolean(label: 'Show titles', initial: true);
+
+  final width = context.knobs.sliderInt(
+    label: 'Column width',
+    initial: 300,
+    max: 400,
+  );
+
+  return SingleChildScrollView(
+    padding: const EdgeInsets.all(24),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        for (var e in elements.entries) ...[
+          const ExpandedDivider(),
+          if (showTitles) Text(e.key),
+          SignInWidgetsColumn(
+            spacing: 8,
+            width: width.toDouble(),
+            children: e.value,
+          ),
+          if (e.key == elements.entries.last.key) const ExpandedDivider(),
+        ],
+      ],
+    ),
+  );
+}
+
+extension EnumAsOptionsList<T extends Enum> on Iterable<T> {
+  List<Option<T>> asOptions() {
+    return map((e) => Option<T>(label: e.name, value: e)).toList();
+  }
+}
