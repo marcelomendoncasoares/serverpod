edcce90a9d91fc234e73fbaa7f306ac507c7b032
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-02-24 21:44:34 -0300

    feat: Introduce the `SqliteDatabaseConfig`

--- packages/serverpod_shared/lib/src/environment_variables.dart
+++ packages/serverpod_shared/lib/src/environment_variables.dart
@@ -75,6 +75,9 @@ enum ServerpodEnv {
   /// The dialect of the database.
   databaseDialect,
 
+  /// The file path to the SQLite database.
+  databaseFilePath,
+
   /// The address to the redis broker.
   redisHost,
 
@@ -195,6 +198,7 @@ enum ServerpodEnv {
       (ServerpodEnv.databaseIsUnixSocket) => 'isUnixSocket',
       (ServerpodEnv.databaseMaxConnectionCount) => 'maxConnectionCount',
       (ServerpodEnv.databaseDialect) => 'dialect',
+      (ServerpodEnv.databaseFilePath) => 'filePath',
       (ServerpodEnv.redisHost) => 'host',
       (ServerpodEnv.redisPort) => 'port',
       (ServerpodEnv.redisUser) => 'user',
@@ -250,6 +254,7 @@ enum ServerpodEnv {
       (ServerpodEnv.databaseMaxConnectionCount) =>
         'SERVERPOD_DATABASE_MAX_CONNECTION_COUNT',
       (ServerpodEnv.databaseDialect) => 'SERVERPOD_DATABASE_DIALECT',
+      (ServerpodEnv.databaseFilePath) => 'SERVERPOD_DATABASE_FILE_PATH',
       (ServerpodEnv.redisHost) => 'SERVERPOD_REDIS_HOST',
       (ServerpodEnv.redisPort) => 'SERVERPOD_REDIS_PORT',
       (ServerpodEnv.redisUser) => 'SERVERPOD_REDIS_USER',
